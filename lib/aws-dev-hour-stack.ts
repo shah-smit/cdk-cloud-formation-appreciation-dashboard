3c21e4656ffdf3377e28e3a80d3994e2fc038c0e
Smit <smitshah95@hotmail.com>
2021-08-22 02:17:55 +0800

    Module 5 completed with adding the bucket integration for public folder and fixed the s3 integration with frontend

--- lib/aws-dev-hour-stack.ts
+++ lib/aws-dev-hour-stack.ts
@@ -1,45 +1,91 @@
 import * as cdk from '@aws-cdk/core';
-
-import s3 = require('@aws-cdk/aws-s3')
-import lambda = require('@aws-cdk/aws-lambda')
-import dynamodb = require('@aws-cdk/aws-dynamodb')
-import {Duration, CfnOutput} from '@aws-cdk/core'
-import iam = require('@aws-cdk/aws-iam')
-import event_sources = require('@aws-cdk/aws-lambda-event-sources')
-import apigateway  = require('@aws-cdk/aws-apigateway')
+import s3 = require('@aws-cdk/aws-s3');
+import iam = require('@aws-cdk/aws-iam');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import cognito = require('@aws-cdk/aws-cognito');
 import { AuthorizationType, PassthroughBehavior } from '@aws-cdk/aws-apigateway';
-import cognito = require('@aws-cdk/aws-cognito')
+import { CfnOutput } from "@aws-cdk/core";
+import { Duration } from '@aws-cdk/core';
+import apigw = require('@aws-cdk/aws-apigateway');
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import { HttpMethods } from '@aws-cdk/aws-s3';
 
-
-
-
-const imageBucketName = "cdk-rekn-imagebucket-smit"
+const imageBucketName = "cdk-rekn-imgagebucket"
 const resizedBucketName = imageBucketName + "-resized"
+const websiteBucketName = "cdk-rekn-publicbucket"
 
 export class AwsDevHourStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // =====================================================================================
+    // Image Bucket
+    // =====================================================================================
     const imageBucket = new s3.Bucket(this, imageBucketName, {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
-    new cdk.CfnOutput(this, 'imageBucket', {value: imageBucket.bucketName})
+      autoDeleteObjects: true
+    });
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
     const imageBucketArn = imageBucket.bucketArn;
-
+    imageBucket.addCorsRule({
+      allowedMethods: [HttpMethods.GET, HttpMethods.PUT],
+      allowedOrigins: ["*"],
+      allowedHeaders: ["*"],
+      maxAge: 3000
+    });
+    
+    // =====================================================================================
+    // Thumbnail Bucket
+    // =====================================================================================
     const resizedBucket = new s3.Bucket(this, resizedBucketName, {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    })
-    new cdk.CfnOutput(this, 'resizedBucket', {value: resizedBucket.bucketName})
+      autoDeleteObjects: true
+    });
+    new cdk.CfnOutput(this, 'resizedBucket', {value: resizedBucket.bucketName});
     const resizedBucketArn = resizedBucket.bucketArn;
-
-    const table  = new dynamodb.Table(this, "ImageLabels",  {
-      partitionKey: {name: "image", type: dynamodb.AttributeType.STRING},
+    resizedBucket.addCorsRule({
+      allowedMethods: [HttpMethods.GET, HttpMethods.PUT],
+      allowedOrigins: ["*"],
+      allowedHeaders: ["*"],
+      maxAge: 3000
+    });
+    
+    // =====================================================================================
+    // Construct to create our Amazon S3 Bucket to host our website
+    // =====================================================================================
+    const webBucket = new s3.Bucket(this, websiteBucketName, {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+    
+    webBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [webBucket.arnForObjects('*')],
+      principals: [new iam.AnyPrincipal()]
+      
+    }))
+    new cdk.CfnOutput(this, 'bucketURL', { value: webBucket.bucketWebsiteDomainName });
+    
+    // =====================================================================================
+    // Deploy site contents to S3 Bucket
+    // =====================================================================================
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+        sources: [ s3deploy.Source.asset('./public') ],
+        destinationBucket: webBucket
+    });
+    
+    // =====================================================================================
+    // Amazon DynamoDB table for storing image labels
+    // =====================================================================================
+    const table = new dynamodb.Table(this, 'ImageLabels', {
+      partitionKey: { name: 'image', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY
-    })
-    new cdk.CfnOutput(this, "ddbtable", {value: table.tableName})
+    });
+    new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
 
     // =====================================================================================
     // Building our AWS Lambda Function; compute for our serverless microservice
@@ -50,8 +96,11 @@ export class AwsDevHourStack extends cdk.Stack {
       license: 'Apache-2.0',
       description: 'A layer to enable the PIL library in our Rekognition Lambda',
     });
-
-    const rekFn = new lambda.Function(this, "rekognitionFunction", {
+    ​
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const rekFn = new lambda.Function(this, 'rekognitionFunction', {
       code: lambda.Code.fromAsset('rekognitionlambda'),
       runtime: lambda.Runtime.PYTHON_3_7,
       handler: 'index.handler',
@@ -59,23 +108,27 @@ export class AwsDevHourStack extends cdk.Stack {
       memorySize: 1024,
       layers: [layer],
       environment: {
-        "TABLE": table.tableName,
-        "BUCKET": imageBucket.bucketName,
-        "THUMBBUCKET": resizedBucket.bucketName
-      }
-    })
-
-    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, {events: [s3.EventType.OBJECT_CREATED]}))
-    imageBucket.grantRead(rekFn)
-    table.grantWriteData(rekFn)
-    resizedBucket.grantPut(rekFn)
+          "TABLE": table.tableName,
+          "BUCKET": imageBucket.bucketName,
+          "THUMBBUCKET": resizedBucket.bucketName
+      },
+    });
+    
+    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [ s3.EventType.OBJECT_CREATED ]}));
+    imageBucket.grantRead(rekFn);
+    resizedBucket.grantPut(rekFn);
+    table.grantWriteData(rekFn);
 
     rekFn.addToRolePolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       actions: ['rekognition:DetectLabels'],
       resources: ['*']
-    }))
-
+    }));
+    
+    // =====================================================================================
+    // Lambda for Synchronous Front End
+    // =====================================================================================
+  ​
     const serviceFn = new lambda.Function(this, 'serviceFunction', {
       code: lambda.Code.fromAsset('servicelambda'),
       runtime: lambda.Runtime.PYTHON_3_7,
@@ -84,67 +137,104 @@ export class AwsDevHourStack extends cdk.Stack {
         "TABLE": table.tableName,
         "BUCKET": imageBucket.bucketName,
         "THUMBBUCKET": resizedBucket.bucketName
-      }
-    })
-
-    imageBucket.grantWrite(serviceFn)
-    resizedBucket.grantWrite(serviceFn)
-    table.grantReadWriteData(serviceFn)
-
-    const api = new apigateway.LambdaRestApi(this, 'imageAPI', {
+      },
+    });
+    ​
+    imageBucket.grantWrite(serviceFn);
+    resizedBucket.grantWrite(serviceFn);
+    table.grantReadWriteData(serviceFn);
+    
+    const api = new apigw.LambdaRestApi(this, 'imageAPI', {
       defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS
       },
       handler: serviceFn,
-      proxy: false
-    })
-
-
-    const userpool = new cognito.UserPool(this, 'UserPool', {
-      selfSignUpEnabled: true,
-      autoVerify: { email: true },
-      signInAliases: { username: true, email: true}
-    })
-
-    const userpoolclient = new cognito.UserPoolClient(this, 'UserPoolClient', {
-      userPool: userpool,
-      generateSecret: false
-    })
-
-    const identitypool = new cognito.CfnIdentityPool(this, 'ImageRekognitionIdentityPool', {
-      allowUnauthenticatedIdentities: false,
-      cognitoIdentityProviders: [
+      proxy: false,
+    });
+    
+    // =====================================================================================
+    // This construct builds a new Amazon API Gateway with AWS Lambda Integration
+    // =====================================================================================
+    const lambdaIntegration = new apigw.LambdaIntegration(serviceFn, {
+      proxy: false,
+      requestParameters: {
+        'integration.request.querystring.action': 'method.request.querystring.action',
+        'integration.request.querystring.key': 'method.request.querystring.key'
+      },
+      requestTemplates: {
+        'application/json': JSON.stringify({ action: "$util.escapeJavaScript($input.params('action'))", key: "$util.escapeJavaScript($input.params('key'))" })
+      },
+      passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+      integrationResponses: [
         {
-          clientId: userpoolclient.userPoolClientId,
-          providerName: userpool.userPoolProviderName
-        }
-      ]
-    })
-
-    const auth = new apigateway.CfnAuthorizer(this, 'APIGatewayAuthorizer', {
-      name: 'customer-authorizer',
-      identitySource: 'method.request.header.Authorization',
-      providerArns: [userpool.userPoolArn],
-      restApiId: api.restApiId,
-      type: AuthorizationType.COGNITO
-    })
-
-    const authenticatedRole = new iam.Role(this, 'ImageRekognitionAuthenticatedRole', {
-      assumedBy: new iam.FederatedPrincipal(
-        'cognito-identity.amazonaws.com', 
-        {
-          StringEquals: {
-            "cognito-identity.amazon.com:aud": identitypool.ref
-          },
-          "ForAnyValue:StringLike": {
-            "cognito-identity.amazonaws.com:amr": "authenticated"
+          statusCode: "200",
+          responseParameters: {
+            // We can map response parameters
+            // - Destination parameters (the key) are the response parameters (used in mappings)
+            // - Source parameters (the value) are the integration response parameters or expressions
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
           }
         },
-        'sts:AssumeRoleWithWebIdentity'
-      )
-    })
+        {
+          // For errors, we check if the error message is not empty, get the error data
+          selectionPattern: "(\n|.)+",
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
+          }
+        }
+      ],
+    });
+    
+    // =====================================================================================
+    // Cognito User Pool Authentication
+    // =====================================================================================
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { username: true, email: true }, // Set email as an alias
+    });
 
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool,
+      generateSecret: false, // Don't need to generate secret for web app running on browsers
+    });
+
+    const identityPool = new cognito.CfnIdentityPool(this, "ImageRekognitionIdentityPool", {
+      allowUnauthenticatedIdentities: false, // Don't allow unathenticated users
+      cognitoIdentityProviders: [
+        {
+        clientId: userPoolClient.userPoolClientId,
+        providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    const auth = new apigw.CfnAuthorizer(this, 'APIGatewayAuthorizer', {
+      name: 'customer-authorizer',
+      identitySource: 'method.request.header.Authorization',
+      providerArns: [userPool.userPoolArn],
+      restApiId: api.restApiId,
+      type: AuthorizationType.COGNITO,
+    });
+
+    const authenticatedRole = new iam.Role(this, "ImageRekognitionAuthenticatedRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+          {
+          StringEquals: {
+              "cognito-identity.amazonaws.com:aud": identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+    });
+
+    // IAM policy granting users permission to upload, download and delete their own pictures
     authenticatedRole.addToPolicy(
       new iam.PolicyStatement({
         actions: [
@@ -175,55 +265,31 @@ export class AwsDevHourStack extends cdk.Stack {
     );
 
     new cognito.CfnIdentityPoolRoleAttachment(this, "IdentityPoolRoleAttachment", {
-      identityPoolId: identitypool.ref,
+      identityPoolId: identityPool.ref,
       roles: { authenticated: authenticatedRole.roleArn },
     });
 
+    // Export values of Cognito
     new CfnOutput(this, "UserPoolId", {
-      value: userpool.userPoolId,
+      value: userPool.userPoolId,
     });
     new CfnOutput(this, "AppClientId", {
-      value: userpoolclient.userPoolClientId,
+      value: userPoolClient.userPoolClientId,
     });
     new CfnOutput(this, "IdentityPoolId", {
-      value: identitypool.ref,
+      value: identityPool.ref,
     });
-    
 
-    const lambdaIntegration = new apigateway.LambdaIntegration(serviceFn, {
-      proxy: false,
-      requestParameters: {
-        'integration.request.querystring.action': 'method.request.querystring.action',
-        'integration.request.querystring.key': 'method.request.querystring.key',
-      },
 
-      requestTemplates: {
-        'application/json': JSON.stringify({ action: "$util.escapeJavaScript($input.params('action'))", key: "$util.escapeJavaScript($input.params('key'))" })
-      },
-
-      passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
-      integrationResponses: [
-        {
-          statusCode: "200",
-          responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': "'*'"
-          }
-        },
-        {
-          selectionPattern: "(\n|.)+",
-          statusCode: "500",
-          responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': "'*'"
-          }
-        }
-      ]
-    })
-
+    // =====================================================================================
+    // API Gateway
+    // =====================================================================================
     const imageAPI = api.root.addResource('images');
-
+    ​
+    // GET /images
     imageAPI.addMethod('GET', lambdaIntegration, {
       authorizationType: AuthorizationType.COGNITO,
-      authorizer: { authorizerId: auth.ref},
+      authorizer: { authorizerId: auth.ref },
       requestParameters: {
         'method.request.querystring.action': true,
         'method.request.querystring.key': true
@@ -232,21 +298,22 @@ export class AwsDevHourStack extends cdk.Stack {
         {
           statusCode: "200",
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': true
-          }
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
         },
         {
           statusCode: "500",
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': true
-          }
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
         }
       ]
-    })
-
+    });
+    
+    // DELETE /images
     imageAPI.addMethod('DELETE', lambdaIntegration, {
       authorizationType: AuthorizationType.COGNITO,
-      authorizer: { authorizerId: auth.ref},
+      authorizer: { authorizerId: auth.ref },
       requestParameters: {
         'method.request.querystring.action': true,
         'method.request.querystring.key': true
@@ -255,18 +322,17 @@ export class AwsDevHourStack extends cdk.Stack {
         {
           statusCode: "200",
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': true
-          }
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
         },
         {
           statusCode: "500",
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Origin': true
-          }
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
         }
       ]
-    })
-
-
+    });
+  
   }
 }
