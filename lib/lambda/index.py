452eb91d0a189e4f12c81dbae527081c54665259
Smit <smitshah95@hotmail.com>
2021-09-05 01:19:46 +0800

    Added dynamodb config in lambda

--- lib/lambda/index.py
+++ lib/lambda/index.py
@@ -1,8 +1,12 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Messages')
+    data = table.scan()
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(data)
     }
