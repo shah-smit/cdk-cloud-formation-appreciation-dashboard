1dac6d05b38bc5c37515bd923fe88bf8119778f6
Smit <smitshah95@hotmail.com>
2021-09-12 14:39:08 +0800

    Added basic tests for the infrastructure creation

--- bin/cdk-cloud-formation-appreciation-dashboard.ts
+++ bin/cdk-cloud-formation-appreciation-dashboard.ts
@@ -2,8 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkCloudFormationAppreciationDashboardPipelineStack } from '../lib/cdk-cloud-formation-appreciation-dashboard-pipeline';
+import { CdkCloudFormationAppreciationDashboardStack } from '../lib/cdk-cloud-formation-appreciation-dashboard-stack';
 
 const app = new cdk.App();
+new CdkCloudFormationAppreciationDashboardStack(app, 'DashboardStack');
 new CdkCloudFormationAppreciationDashboardPipelineStack(app, 'CdkCloudFormationAppreciationDashboardPipelineStack', {
   env: { account: '174428063264', region: 'ap-southeast-1' },
 });
--- test/cdk-cloud-formation-appreciation-dashboard.test.ts
+++ test/cdk-cloud-formation-appreciation-dashboard.test.ts
@@ -1,13 +1,50 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource, stringLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkCloudFormationAppreciationDashboard from '../lib/cdk-cloud-formation-appreciation-dashboard-stack';
 
-test('Empty Stack', () => {
+test('Read-Lambda Created', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkCloudFormationAppreciationDashboard.CdkCloudFormationAppreciationDashboardStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    //Get Table Name
+    
+    expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
+      "Handler": "index.lambda_handler",
+      Environment: {
+        Variables: {
+          TABLE: {
+            Ref: stringLike("Messages*"),
+          }
+        }
+      }
+      }));
 });
+
+test('DynamoDB Table Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkCloudFormationAppreciationDashboard.CdkCloudFormationAppreciationDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::DynamoDB::Table",{
+    "AttributeDefinitions": [
+      {
+        "AttributeName": "messsage",
+        "AttributeType": "S"
+      }
+    ],
+    "ProvisionedThroughput": {
+      "ReadCapacityUnits": 5,
+      "WriteCapacityUnits": 5
+    }
+  }));
+});
+
+test('API Gateway Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkCloudFormationAppreciationDashboard.CdkCloudFormationAppreciationDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::ApiGateway::RestApi"));
+});
+
